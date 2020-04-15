15fe0fd614ef5b0d6437c4c6b9690026b365aff7
balczarlajos <>
2020-04-16 00:57:20 +0200

    added backend support for bid requests on an item

--- backend/modules/bid/bid.controller.ts
+++ backend/modules/bid/bid.controller.ts
@@ -1,10 +1,25 @@
 import Bid from './bid.entity';
 import CRUDController from '../crud/crud.controller';
-
+import BidModel from './bid.model';
+import Item from '../item/item.entity';
+import * as jwt from 'jsonwebtoken';
+import { tokenSecret } from '../../shared/auth/configs';
 
 class BidController extends CRUDController<Bid> {
   constructor() {
     super('bid');
+
+    this.model = new BidModel();
+  }
+
+  async GetItemBids(request, response) {
+    const item: Item = request.body;
+
+    const foundBids = await this.model.findByItemId(item);
+
+    response.send({
+      foundBids
+    });
   }
 }
 
--- backend/modules/bid/bid.model.ts
+++ backend/modules/bid/bid.model.ts
@@ -1,5 +1,27 @@
-class BidModel {
+import CRUDModel from '../crud/crud.model';
+import Bid from './bid.entity';
+import Item from '../item/item.entity';
+import Database, { Repository } from '../../shared/database/database';
 
+export interface BidModelInterface {
+    repository: Repository<Bid>;
+    findByItemId: (object: Item) => Promise<Bid[]>;
+}
+
+class BidModel extends CRUDModel<Bid> implements BidModelInterface {
+    repository: Repository<Bid>;
+
+    constructor() {
+        super('bid');
+
+        new Database().connect().then((connection) => {
+          this.repository = connection.getRepository(Bid);
+        });
+    }
+
+    async findByItemId(item: Item): Promise<Bid[]> {
+       return this.repository.find({ where: { itemId: item.id } });
+    }
 }
 
 export default BidModel;
